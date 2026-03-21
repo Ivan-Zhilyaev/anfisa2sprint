d07d6c61984744b7222b0fe1736d8aa79638bb71
Ivan Zhilyaev <iv.zhilyaev@yandex.ru>
2026-03-21 11:37:31 +0300

    Запись данных в БД через форму.

--- anfisa_for_friends/contest/views.py
+++ anfisa_for_friends/contest/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
 
 from .forms import ContestForm
+from .models import Contest
 
 def proposal_create(request):
     form = ContestForm(request.POST or None)
     # Создаём словарь контекста сразу после инициализации формы.
     context = {'form': form}
+    if form.is_valid():
+        form.save()
     return render(request, 'contest/form.html', context)
 
 
 def accepted(request):
-    return render(request, 'contest/proposal_accepted.html')
+    bids = Contest.objects.all().order_by('id')
+    context = {'bids': bids}
+    return render(request, 'contest/proposal_accepted.html', context)
